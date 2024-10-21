b0d2ff5b8970ccc31701c899d943fb231edc85dc
bachie <elijah99lil@gmail.com>
2024-10-21 04:09:15 -0600

    VII: added FPS and optimized 'main()' in main.py!

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick/1000
     
 if __name__ == "__main__":
     main()
