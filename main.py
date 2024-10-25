80be72382615ac4e747f2936796977a4b81b8d0b
bachie <elijah99lil@gmail.com>
2024-10-25 03:23:37 -0600

    X: got A and D to turn the ship

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def main():
     dt = 0
     
     while True:
+        dt = clock.tick(60) / 1000.0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         tick = clock.tick(60)
-        dt = tick/1000
     
 if __name__ == "__main__":
     main()
