7f224f0f91822609f0bd1d42762902749158da93
bachie <elijah99lil@gmail.com>
2024-10-26 04:34:32 -0600

    XIV: added shot.py and got the ship shootin'

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
 import sys
+from shot import Shot
 
 def main():
     pygame.init()
@@ -14,8 +15,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
